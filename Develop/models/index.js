28ab12a9eefef75fb0f5e344b2a6432b1f84ccd5
Tank <johnson90.cm@gmail.com>
2021-12-10 08:43:57 -0700

    inital index setup

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Category, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+});
 
 module.exports = {
   Product,
